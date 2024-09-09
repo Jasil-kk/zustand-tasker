c6774882387a2786afd13005cd5db33904fe2e31
Jasil-kk <jasikk2522@gmail.com>
2024-09-09 10:53:44 +0530

    all,completed,pending tasks option added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import useTaskStore from "./store";
 
@@ -6,6 +6,7 @@ function App() {
   const [newTask, setNewTask] = useState("");
   const [editTaskId, setEditTaskId] = useState(null);
   const [editTaskTitle, setEditTaskTitle] = useState("");
+  const [tab, setTab] = useState("All");
   const inputRef = useRef();
 
   const tasks = useTaskStore((state) => state.tasks);
@@ -51,6 +52,15 @@ function App() {
     }
   };
 
+  const filteredTasks = tasks.filter((task) => {
+    if (tab === "Completed") {
+      return task.completed;
+    } else if (tab === "Pending") {
+      return !task.completed;
+    }
+    return true;
+  });
+
   return (
     <div className="main">
       <h1>Taks Management</h1>
@@ -70,53 +80,87 @@ function App() {
           {editTaskId ? "Save Changes" : "Add Task"}
         </button>
       </div>
-      <ul>
-        {tasks.toReversed().map((task) => (
-          <li key={task.id}>
-            <div className="checkbox-wrapper-30">
-              <span className="checkbox">
-                <input
-                  type="checkbox"
-                  checked={task.completed}
-                  onChange={() => toggleTaskStatus(task.id)}
-                />
-                <svg>
-                  <use xlinkHref="#checkbox-30" className="checkbox"></use>
-                </svg>
-              </span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                style={{ display: "none" }}
-              >
-                <symbol id="checkbox-30" viewBox="0 0 22 22">
-                  <path
-                    fill="none"
-                    stroke="currentColor"
-                    d="M5.5,11.3L9,14.8L20.2,3.3l0,0c-0.5-1-1.5-1.8-2.7-1.8h-13c-1.7,0-3,1.3-3,3v13c0,1.7,1.3,3,3,3h13 c1.7,0,3-1.3,3-3v-13c0-0.4-0.1-0.8-0.3-1.2"
-                  />
-                </symbol>
-              </svg>
-            </div>
-            <p
-              style={{
-                textDecoration: task.completed ? "line-through" : "none",
-              }}
-            >
-              {task.title}
-            </p>
-            <button className="edit_btn" onClick={() => handleEdit(task)}>
-              Edit
-            </button>
-            <button
-              disabled={editTaskId === task.id}
-              className="delete_btn"
-              onClick={() => deleteTask(task.id)}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
+      <ul className="tabs_container">
+        <li
+          onClick={() => setTab("All")}
+          className={tab === "All" ? "active" : ""}
+        >
+          All
+        </li>
+        <li
+          onClick={() => setTab("Completed")}
+          className={tab === "Completed" ? "active" : ""}
+        >
+          Completed
+        </li>
+        <li
+          onClick={() => setTab("Pending")}
+          className={tab === "Pending" ? "active" : ""}
+        >
+          Pending
+        </li>
       </ul>
+      {filteredTasks && filteredTasks.length > 0 ? (
+        <ul className="taks_list_container">
+          {filteredTasks.toReversed().map((task) => (
+            <li key={task.id}>
+              <div className="checkbox-wrapper-30">
+                <span className="checkbox">
+                  <input
+                    type="checkbox"
+                    checked={task.completed}
+                    onChange={() => toggleTaskStatus(task.id)}
+                  />
+                  <svg>
+                    <use xlinkHref="#checkbox-30" className="checkbox"></use>
+                  </svg>
+                </span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  style={{ display: "none" }}
+                >
+                  <symbol id="checkbox-30" viewBox="0 0 22 22">
+                    <path
+                      fill="none"
+                      stroke="currentColor"
+                      d="M5.5,11.3L9,14.8L20.2,3.3l0,0c-0.5-1-1.5-1.8-2.7-1.8h-13c-1.7,0-3,1.3-3,3v13c0,1.7,1.3,3,3,3h13 c1.7,0,3-1.3,3-3v-13c0-0.4-0.1-0.8-0.3-1.2"
+                    />
+                  </symbol>
+                </svg>
+              </div>
+              <p
+                style={{
+                  textDecoration: task.completed ? "line-through" : "none",
+                }}
+              >
+                {task.title}
+              </p>
+              <button
+                disabled={task.completed}
+                className="edit_btn"
+                onClick={() => handleEdit(task)}
+              >
+                Edit
+              </button>
+              <button
+                disabled={editTaskId === task.id || task.completed}
+                className="delete_btn"
+                onClick={() => deleteTask(task.id)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="notfound">
+          {tab === "Completed"
+            ? "No completed tasks found"
+            : tab === "Pending"
+            ? "No pending tasks found"
+            : "No tasks found"}
+        </div>
+      )}
     </div>
   );
 }
